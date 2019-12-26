9ece91a669fdb393c257669db77f792452d53313
NickLarsenNZ <nick@aptiv.co.nz>
2019-12-26 15:59:49 +0330

    Commented on repository save return type

--- src/main/kotlin/domain/repository/Repository.kt
+++ src/main/kotlin/domain/repository/Repository.kt
@@ -4,5 +4,5 @@ import java.util.*
 
 interface Repository<T> {
     fun findById(id: UUID): T
-    fun save(id: UUID, domainEntity: T)
+    fun save(domainEntity: T) // Todo: should this have a return type? If so, should it be a Boolean or T?
 }
